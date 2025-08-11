4e7d37fafd7ba4e90afafcd0b595d5fb6db263c7
vana1219 <71721931+vana1219@users.noreply.github.com>
2025-08-11 14:09:51 -0700

    update some change

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import path from "path"
 
 import noteRoute from './routes/noteRoute.js'
 import { connectDB } from './config/db.js'
@@ -11,19 +12,34 @@ dotenv.config();
 
 const app = express()
 const PORT = process.env.PORT || 3000
+const __dirname=path.resolve()
 
-connectDB()
 
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV!=="production"){
+    app.use(cors({
     origin: "http://localhost:5173"
 }))
+
+}
+
 app.use(express.json())
 app.use(rateLimiter)
 
 
 app.use('/api/notes', noteRoute)
-app.listen(PORT, ()=>{
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req,res)=>{
+        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
+    });
+}
+
+
+connectDB().then(()=>{
+    app.listen(PORT, ()=>{
     console.log('Server is running on port 3000')
 })
-
+});
